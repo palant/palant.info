621e6039966686b0adc1ddbb7afd941e2c5928d0
Wladimir Palant <fqcgithub@palant.de>
2019-04-02 10:41:20 +0200

    Added JavaScript logic to handle comments

---
+++ static/js/comments.js
@@ -0,0 +1,70 @@
+(function initComments()
+{
+  function hideError()
+  {
+    var error = document.getElementById("comment-error");
+    if (error)
+      error.parentNode.removeChild(error);
+  }
+
+  function showError(text)
+  {
+    hideError();
+
+    var error = document.createElement("p");
+    error.id = "comment-error";
+    error.className = "comments_error";
+    error.textContent = text;
+
+    var insertionPoint = document.getElementById("comment-submit");
+    insertionPoint.parentNode.insertBefore(error, insertionPoint.nextSibling);
+
+    error.scrollIntoView(true);
+  }
+
+  function showSuccess(text)
+  {
+    document.getElementById("comments").textContent = text;
+  }
+
+  var form = document.getElementById("comments");
+  form.addEventListener("submit", function(event)
+  {
+    event.preventDefault();
+    hideError();
+
+    var url = "/comment/submit";
+    if (location.hostname == "localhost")
+      url = "http://localhost:5000" + url;
+
+    var request = new XMLHttpRequest();
+    request.open("POST", url);
+    request.addEventListener("load", function()
+    {
+      if (request.status != 200)
+        return showError("Unexpected server response: " + request.status);
+
+      var response;
+      try
+      {
+        response = JSON.parse(request.responseText);
+      }
+      catch (e)
+      {
+        return showError("Invalid server response.");
+      }
+
+      if (response.error)
+        return showError(response.message);
+
+      return showSuccess(response.message);
+    }, false);
+
+    request.addEventListener("error", function()
+    {
+      return showError("Connection error.");
+    }, false);
+
+    request.send(new FormData(form));
+  }, false);
+})();
